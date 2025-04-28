d30ff2c94702c7e37b473f6f588a76a05f9cb7c0
Felipe Panosso <felipepanosso21@gmail.com>
2025-04-28 10:24:27 -0300

    feat:Create all the initial structure and configuration and DTO classes for data exchange between the front-end and back-end

---
+++ shipping/src/main/java/redirex/shipping/dto/AuthRequestDTO.java
@@ -0,0 +1,9 @@
+package redirex.shipping.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthRequestDTO {
+    private String email;
+    private String password;
+}
---
+++ shipping/src/main/java/redirex/shipping/entity/User.java
@@ -0,0 +1,56 @@
+package redirex.shipping.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String fullname;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String complement;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String zipcode;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String occupation;
+
+    private String role;
+}
