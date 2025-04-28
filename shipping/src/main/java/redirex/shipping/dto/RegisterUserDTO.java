fd5dd7e9b55b56e61aa7c01c804faf8103121e81
Felipe Panosso <felipepanosso21@gmail.com>
2025-04-28 10:44:09 -0300

    feat: Implements the logic for creating a new user and verifying their login. The Userservice manages the user creation flow and encodes their passwords. It uses the PasswordEncoder to encrypt the password before saving it to the database.

---
+++ shipping/src/main/java/redirex/shipping/dto/RegisterUserDTO.java
@@ -0,0 +1,9 @@
+package redirex.shipping.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterUserDTO {
+    private String email;
+    private String password;
+}
--- shipping/src/main/java/redirex/shipping/service/UserService.java
+++ shipping/src/main/java/redirex/shipping/service/UserService.java
@@ -1,4 +1,25 @@
 package redirex.shipping.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import redirex.shipping.dto.RegisterUserDTO;
+import redirex.shipping.entity.User;
+import redirex.shipping.repositories.UserRepository;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User register(RegisterUserDTO dto) {
+        User user = User.builder()
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .role("ROLE_USER")
+                .build();
+        return userRepository.save(user);
+    }
 }
