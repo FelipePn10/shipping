3e4c212a0c2c08d4ff1ebf945d0a737052a316f3
Felipe Panosso <felipepanosso21@gmail.com>
2025-04-28 09:34:38 -0300

    Initial commit

---
+++ shipping/src/test/java/redirex/shipping/GlobalApplicationTests.java
@@ -0,0 +1,13 @@
+package redirex.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
