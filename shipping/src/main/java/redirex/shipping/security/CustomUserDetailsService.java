77e91cbf9ed78d92d4b8bb6bd685378243c4cfbb
Felipe Panosso <felipepanosso21@gmail.com>
2025-04-28 14:04:26 -0300

    feat: Spring Security integration with custom authentication logic.

---
+++ shipping/src/main/java/redirex/shipping/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package redirex.shipping.security;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import redirex.shipping.entity.User;
+import redirex.shipping.repositories.UserRepository;
+
+import java.util.ArrayList;
+
+@Service
+public abstract class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
+
+// CustomUserDetailsService permite que o Spring Security carregue informações sobre o usuário a partir do banco de dados. E o método loadUserByUsername recupera o usuário pelo email.
