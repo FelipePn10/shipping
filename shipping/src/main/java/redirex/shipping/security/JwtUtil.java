84fb4a8f064b5d04a9acf589354256e291635d31
Felipe Panosso <felipepanosso21@gmail.com>
2025-04-28 13:59:47 -0300

    feat: Implementation of JWT token creation and validation functions

---
+++ shipping/src/main/java/redirex/shipping/security/JwtUtil.java
@@ -0,0 +1,29 @@
+package redirex.shipping.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    private String secretKey = "SuperSegredoJWT12345!";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new java.util.Date())
+                .setExpiration(new java.util.Date((new java.util.Date()).getTime() + 1000 * 60 * 60 * 24))
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        return !Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new java.util.Date());
+    }
+}
