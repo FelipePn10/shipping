65515c59e45bf3f5bf98640cc3a842a9d3fe977e
Felipe Panosso <felipepanosso21@gmail.com>
2025-06-13 13:05:49 -0300

    feat: Added three fields to the response of an order (OrderItemResponse). The fields are: Id, description and size

--- shipping/src/main/java/redirex/shipping/dto/response/OrderItemResponse.java
+++ shipping/src/main/java/redirex/shipping/dto/response/OrderItemResponse.java
@@ -2,15 +2,21 @@ package redirex.shipping.dto.response;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import redirex.shipping.enums.CurrencyEnum;
 import redirex.shipping.enums.OrderItemStatusEnum;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Builder
 public class OrderItemResponse {
+    private Long id;
+    private String description;
+    private Float size;
     private String productUrl;
     private BigDecimal productValue;
     private CurrencyEnum originalCurrency;
